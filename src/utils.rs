48d5a5d930b3e3068ae574f9c8a2bed8264b4b51
khadievedem <khadiev.edem@gmail.com>
2023-04-22 21:52:59 +0300

    input as in contest

--- src/utils.rs
+++ src/utils.rs
@@ -1,21 +1,21 @@
 use crate::algorithms::structs::{point::Point, rect::Rect};
 
 pub struct InputData {
-    pub p: Point,
+    pub points: Vec<Point>,
     pub rects: Vec<Rect>,
 }
 
 impl InputData {
-    fn new(p: Point, rects: Vec<Rect>) -> Self {
+    fn new(points: Vec<Point>, rects: Vec<Rect>) -> Self {
         Self {
-            p: (p),
+            points: (points),
             rects: (rects),
         }
     }
 }
 
-impl From<(Point, Vec<Rect>)> for InputData {
-    fn from(value: (Point, Vec<Rect>)) -> Self {
+impl From<(Vec<Point>, Vec<Rect>)> for InputData {
+    fn from(value: (Vec<Point>, Vec<Rect>)) -> Self {
         Self::new(value.0, value.1)
     }
 }
@@ -24,32 +24,58 @@ pub(super) fn user_input() -> std::io::Result<InputData> {
     let mut buf = String::new();
     let stdin = std::io::stdin();
 
-    print!("Enter number of rectangles: ");
+    println!("Enter number of rectangles: ");
     stdin.read_line(&mut buf)?;
-    let n: i32 = buf.parse().unwrap();
+    let n: i32 = buf.strip_suffix('\n').unwrap().parse().unwrap();
+    buf.clear();
     let mut rects: Vec<Rect> = Vec::new();
 
     for i in 0..n {
-        print!("[{i} rect] Enter lower-left rectangle coords (\"x y\"): ");
-        let ll = read_point(&stdin, &mut buf)?;
-
-        print!("[{i} rect] Enter upper-right rectangle coords (\"x y\"): ");
-        let ur = read_point(&stdin, &mut buf)?;
-
-        rects.push(Rect::new(ll, ur));
+        println!(
+            "[{i} rect] Enter lower-left and upper right rectangle coords (\"x1 y1 x2 y2\"): "
+        );
+        let rect = read_rect(&stdin, &mut buf)?;
+        rects.push(rect);
+        buf.clear();
     }
-    print!("Enter point coords (\"x y\"): ");
-    let p = read_point(&stdin, &mut buf)?;
+    println!("Enter number of points: ");
+    stdin.read_line(&mut buf)?;
 
-    Ok(InputData::from((p, rects)))
+    println!("Enter point coords (\"x y\"): ");
+    let m: i32 = buf.strip_suffix('\n').unwrap().parse().unwrap();
+    let mut points: Vec<Point> = Vec::new();
+    buf.clear();
+
+    for i in 0..m {
+        println!("[{i} point] Enter point coords (\"x y\"): ");
+        let p = read_point(&stdin, &mut buf)?;
+        buf.clear();
+        points.push(p);
+    }
+    Ok(InputData::from((points, rects)))
 }
 
 fn read_point(stdin: &std::io::Stdin, buf: &mut String) -> std::io::Result<Point> {
     stdin.read_line(buf)?;
-    let mut p = buf.split(" ");
+    let mut p = buf.strip_suffix('\n').unwrap().split(" ");
     let p: (i32, i32) = (
         p.next().unwrap().parse().unwrap(),
         p.next().unwrap().parse().unwrap(),
     );
     Ok(Point::from_tup(p))
+}
+
+fn read_rect(stdin: &std::io::Stdin, buf: &mut String) -> std::io::Result<Rect> {
+    stdin.read_line(buf)?;
+    let mut p = buf.strip_suffix('\n').unwrap().split(" ");
+    let p1: (i32, i32) = (
+        p.next().unwrap().parse().unwrap(),
+        p.next().unwrap().parse().unwrap(),
+    );
+    let p2: (i32, i32) = (
+        p.next().unwrap().parse().unwrap(),
+        p.next().unwrap().parse().unwrap(),
+    );
+
+    Ok(Rect::new(Point::from_tup(p1), Point::from_tup(p2)))
 }
