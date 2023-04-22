fa44d5c9b0f44e911893d8c30c7a211cb54861d7
khadievedem <khadiev.edem@gmail.com>
2023-04-22 21:54:26 +0300

    use points vector instead of a single point

--- src/algorithms/traits/lab.rs
+++ src/algorithms/traits/lab.rs
@@ -1,5 +1,5 @@
 use crate::algorithms::structs::{point::Point, rect::Rect};
 
 pub trait SecondLabSolution {
-    fn count_rect_for_point(p: &Point, rects: &Vec<Rect>) -> u32;
+    fn count_rect_for_point(p: &Vec<Point>, rects: &Vec<Rect>) -> u32;
 }
