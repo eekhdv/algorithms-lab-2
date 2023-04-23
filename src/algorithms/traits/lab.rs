8149fb775f1ca3d2aa78c196fe1bd3a7d660c4b1
khadievedem <khadiev.edem@gmail.com>
2023-04-23 13:49:32 +0300

    change trait to return None

--- src/algorithms/traits/lab.rs
+++ src/algorithms/traits/lab.rs
@@ -1,5 +1,5 @@
 use crate::algorithms::structs::{point::Point, rect::Rect};
 
-pub trait SecondLabSolution {
-    fn count_rect_for_point(p: &Vec<Point>, rects: &Vec<Rect>) -> u32;
+pub trait LabSolution {
+    fn count_rect_for_point(p: &Vec<Point>, rects: &Vec<Rect>);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,22 @@
-use algorithms::traits::lab::SecondLabSolution;
-
 pub mod algorithms;
 mod utils;
 
+use algorithms::traits::lab::LabSolution;
+
 fn main() -> std::io::Result<()> {
     let input_data = utils::user_input()?;
-    // let _first_ans = algorithms::first::AlgorithmBruteForce::count_rect_for_point(
-    //     &input_data.p,
-    //     &input_data.rects,
-    // );
-    // let _second_ans =
-    //     algorithms::second::AlgorithmOnMap::count_rect_for_point(&input_data.p, &input_data.rects);
-    let _third_ans = algorithms::third::AlgorithmOnPersistenTree::count_rect_for_point(&input_data.points, &input_data.rects);
+    let _first_ans = algorithms::first::AlgorithmBruteForce::count_rect_for_point(
+        &input_data.points,
+        &input_data.rects,
+    );
+    let _second_ans = algorithms::second::AlgorithmOnMap::count_rect_for_point(
+        &input_data.points,
+        &input_data.rects,
+    );
+    let _third_ans = algorithms::third::AlgorithmOnPersistenTree::count_rect_for_point(
+        &input_data.points,
+        &input_data.rects,
+    );
 
     Ok(())
 }
