ed0adb42ef437a68ce15e3b8e15d9ed899653a04
khadievedem <khadiev.edem@gmail.com>
2023-04-25 10:26:22 +0300

    add method for data preparation

--- src/algorithms/traits/lab.rs
+++ src/algorithms/traits/lab.rs
@@ -1,5 +1,6 @@
 use crate::algorithms::structs::{point::Point, rect::Rect};
 
-pub trait LabSolution {
+pub trait LabSolution<T, U> {
     fn count_rect_for_point(p: &Vec<Point>, rects: &Vec<Rect>) -> Vec<i32>;
+    fn prepare_data(_: T) -> Option<U> { None }
 }
