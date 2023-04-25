f7b443d96685bf69da21e67ef696fc30b746d468
khadievedem <khadiev.edem@gmail.com>
2023-04-25 10:29:16 +0300

    add method for running with prepared data

--- src/algorithms/traits/lab.rs
+++ src/algorithms/traits/lab.rs
@@ -2,5 +2,6 @@ use crate::algorithms::structs::{point::Point, rect::Rect};
 
 pub trait LabSolution<T, U> {
     fn count_rect_for_point(p: &Vec<Point>, rects: &Vec<Rect>) -> Vec<i32>;
-    fn prepare_data(_: T) -> Option<U> { None }
+    fn prepare_data(_idata: T) -> Option<U> { None }
+    fn run_with_prepared(prepared_data: U, p: &Vec<Point>) -> Vec<i32>;
 }
--- src/utils.rs
+++ src/utils.rs
@@ -24,30 +24,30 @@ pub(super) fn user_input() -> std::io::Result<InputData> {
     let mut buf = String::new();
     let stdin = std::io::stdin();
 
-    println!("Enter number of rectangles: ");
+    // println!("Enter number of rectangles: ");
     stdin.read_line(&mut buf)?;
     let n: i32 = buf.strip_suffix('\n').unwrap().parse().unwrap();
     buf.clear();
     let mut rects: Vec<Rect> = Vec::new();
 
-    for i in 0..n {
-        println!(
-            "[{i} rect] Enter lower-left and upper right rectangle coords (\"x1 y1 x2 y2\"): "
-        );
+    for _i in 0..n {
+        // println!(
+        //     "[{i} rect] Enter lower-left and upper right rectangle coords (\"x1 y1 x2 y2\"): "
+        // );
         let rect = read_rect(&stdin, &mut buf)?;
         rects.push(rect);
         buf.clear();
     }
-    println!("Enter number of points: ");
+    // println!("Enter number of points: ");
     stdin.read_line(&mut buf)?;
 
-    println!("Enter point coords (\"x y\"): ");
+    // println!("Enter point coords (\"x y\"): ");
     let m: i32 = buf.strip_suffix('\n').unwrap().parse().unwrap();
     let mut points: Vec<Point> = Vec::new();
     buf.clear();
 
-    for i in 0..m {
-        println!("[{i} point] Enter point coords (\"x y\"): ");
+    for _i in 0..m {
+        // println!("[{i} point] Enter point coords (\"x y\"): ");
         let p = read_point(&stdin, &mut buf)?;
         buf.clear();
         points.push(p);
