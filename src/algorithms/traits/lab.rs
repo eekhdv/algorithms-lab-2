cbe80bcddb765cd254cc556535805d9778e3070b
khadievedem <khadiev.edem@gmail.com>
2023-04-25 18:48:54 +0300

    rustfmt

--- src/algorithms/traits/lab.rs
+++ src/algorithms/traits/lab.rs
@@ -2,6 +2,9 @@ use crate::algorithms::structs::{point::Point, rect::Rect};
 
 pub trait LabSolution<T, U> {
     fn count_rect_for_point(p: &Vec<Point>, rects: &Vec<Rect>) -> Vec<i32>;
-    fn prepare_data(_idata: T) -> Option<U> { None }
-    fn run_with_prepared(prepared_data: U, p: &Vec<Point>) -> Vec<i32>;
+    fn prepare_data(_idata: T) -> Option<U> {
+        None
+    }
+    fn find_single_point(prepared_data: &U, p: &Point) -> i32;
+    fn run_with_prepared(prepared_data: &U, p: &Vec<Point>) -> Vec<i32>;
 }
