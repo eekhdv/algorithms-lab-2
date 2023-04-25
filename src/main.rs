fddbbc5faf9c121720b7ab771a3a3b27c8e99baa
khadievedem <khadiev.edem@gmail.com>
2023-04-25 18:49:02 +0300

    God bless.

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,37 @@
 pub mod algorithms;
 mod utils;
 
-use algorithms::traits::lab::LabSolution;
-
 fn main() -> std::io::Result<()> {
-    let input_data = utils::user_input()?;
-    let _first_ans = algorithms::first::AlgorithmBruteForce::count_rect_for_point(
-        &input_data.points,
-        &input_data.rects,
-    );
-    let _second_ans = algorithms::second::AlgorithmOnMap::count_rect_for_point(
-        &input_data.points,
-        &input_data.rects,
-    );
-    let _third_ans = algorithms::third::AlgorithmOnPersistenTree::count_rect_for_point(
-        &input_data.points,
-        &input_data.rects,
-    );
-
+    // let input_data = utils::user_input()?;
+    // let _first_ans = algorithms::first::AlgorithmBruteForce::count_rect_for_point(
+    //     &input_data.points,
+    //     &input_data.rects,
+    // );
+    // let _second_ans = algorithms::second::AlgorithmOnMap::count_rect_for_point(
+    //     &input_data.points,
+    //     &input_data.rects,
+    // );
+    // let _third_ans = algorithms::third::AlgorithmOnPersistenTree::count_rect_for_point(
+    //     &input_data.points,
+    //     &input_data.rects,
+    // );
+    // println!("{:?}", _third_ans);
     Ok(())
 }
 
 #[cfg(test)]
 mod tests {
-    use mod_exp::mod_exp;
-    use algorithms::traits::lab::LabSolution;
+    use std::time::Instant;
 
-    use crate::algorithms::{structs::{point::Point, rect::Rect}, self};
+    use algorithms::traits::lab::LabSolution;
+    use mod_exp::mod_exp;
+
+    use crate::algorithms::{
+        self,
+        structs::{point::Point, rect::Rect},
+    };
+
+    use rand::prelude::*;
 
     struct TestData {
         rects: Vec<Rect>,
@@ -37,10 +42,22 @@ mod tests {
         fn into(self) -> (Vec<Rect>, Vec<Point>) {
             (self.rects, self.points)
         }
-
     }
-    fn generate_tests() -> TestData {
-        let n = 37;
+    #[test]
+    fn data_preparation_test() {
+        data_preparation_test_case(&_generate_small_data(), "small");
+        data_preparation_test_case(&_generate_medium_data(), "medium");
+        data_preparation_test_case(&_generate_huge_data(), "huge");
+    }
+
+    #[test]
+    fn finding_answer_algorithm_test() {
+        finding_answer_test_case(&_generate_small_data(), "small");
+        finding_answer_test_case(&_generate_medium_data(), "medium");
+        finding_answer_test_case(&_generate_medium_data(), "huge");
+    }
+
+    fn generate_tests(n: i32) -> TestData {
         TestData {
             rects: generate_rects(n),
             points: generate_points(n),
@@ -49,12 +66,10 @@ mod tests {
 
     fn generate_rects(n: i32) -> Vec<Rect> {
         let mut rects: Vec<Rect> = Vec::with_capacity(n as usize);
-        for i in 1..37 {
-            let lower_l = Point::new(10 * i, 10 * i);
-            let upper_r = Point::new(10*(2 * n - i), 10 * (2 * n - i));
-            println!("x1: {} y1: {}", lower_l.x, lower_l.y);
-            println!("x2: {} y2: {}", upper_r.x, upper_r.y);
-            rects.push(Rect::new(lower_l, upper_r));
+        for i in 1..n {
+            let ll = Point::new(10 * i, 10 * i);
+            let ur = Point::new(10 * (2 * n - i), 10 * (2 * n - i));
+            rects.push(Rect::new(ll, ur));
         }
         rects
     }
@@ -62,34 +77,100 @@ mod tests {
     fn generate_points(n: i32) -> Vec<Point> {
         let exp = 31;
         let mut points: Vec<Point> = Vec::with_capacity(n as usize);
-        for i in 1..37 {
-            let base_x = 2371 * i;
-            let base_y = 1979 * i;
-            let x: i32 = mod_exp(base_x, exp, 20 * n).try_into().unwrap();
-            let y: i32 = mod_exp(base_y, exp, 20 * n).try_into().unwrap();
+        for i in 1..n {
+            let base_x: i32 = 2371 * i;
+            let base_y: i32 = 1979 * i;
+            let x = mod_exp(base_x as i128, exp as i128, 20 * n as i128) as i32;
+            let y = mod_exp(base_y as i128, exp as i128, 20 * n as i128) as i32;
             points.push(Point::new(x, y));
         }
-        points 
+        points
+    }
+    fn data_preparation_test_case(test_data: &TestData, case: &str) {
+        println!("--- start({}) ---", case);
+        println!(
+            "{} points and {} rectangles were used.",
+            test_data.points.len(),
+            test_data.rects.len()
+        );
+
+        let start = Instant::now();
+        let _second_data_prep = algorithms::second::AlgorithmOnMap::prepare_data(&test_data.rects);
+        let duration = start.elapsed();
+        println!("2 (p) -> time: {} ns", duration.as_nanos());
+
+        let start = Instant::now();
+        let _third_data_prep =
+            algorithms::third::AlgorithmOnPersistenTree::prepare_data(&test_data.rects);
+        let duration = start.elapsed();
+        println!("3 (p) -> time: {} ns", duration.as_nanos());
     }
 
-    #[test]
-    fn firts_algorithm_test() {
-        let _test_data = generate_tests();
+    fn finding_answer_test_case(_test_data: &TestData, case: &str) {
+        println!("--- start({}) ---", case);
+        println!(
+            "{} points and {} rectangles were used.",
+            _test_data.points.len(),
+            _test_data.rects.len()
+        );
 
-        let _first_ans = algorithms::first::AlgorithmBruteForce::count_rect_for_point(
-            &_test_data.points,
-            &_test_data.rects,
-        );
-        let _second_ans = algorithms::second::AlgorithmOnMap::count_rect_for_point(
-            &_test_data.points,
-            &_test_data.rects,
-        );
-        let _third_ans = algorithms::third::AlgorithmOnPersistenTree::count_rect_for_point(
-            &_test_data.points,
-            &_test_data.rects,
-        );
-        assert_eq!(_first_ans, _second_ans);
-        assert_eq!(_first_ans, _third_ans);
+        let print_time = |case_num: &str, time: u128| {
+            println!(
+                "{} (a) -> time per 1 query: {} ns",
+                case_num,
+                time / _test_data.points.len() as u128
+            );
+        };
+
+        let mut all_p_time = 0;
+        let mut ans = 0;
+        for p in &_test_data.points {
+            let start = Instant::now();
+            ans = algorithms::first::AlgorithmBruteForce::find_single_point(&_test_data.rects, p);
+            let duration = start.elapsed();
+            all_p_time += duration.as_nanos();
+        }
+        print_time(&format!("({}) 3", ans), all_p_time);
+
+        if _test_data.rects.len() <= 2000 {
+            let mut all_p_time = 0;
+            let mut ans = 0;
+            let _second_data_prep =
+                algorithms::second::AlgorithmOnMap::prepare_data(&_test_data.rects).unwrap();
+            for p in &_test_data.points {
+                let start = Instant::now();
+                ans += algorithms::second::AlgorithmOnMap::find_single_point(&_second_data_prep, p);
+                let duration = start.elapsed();
+                all_p_time += duration.as_nanos();
+            }
+            print_time(&format!("({}) 3", ans), all_p_time);
+        }
+
+        let mut all_p_time = 0;
+        let mut ans = 0;
+        let _third_data_prep =
+            algorithms::third::AlgorithmOnPersistenTree::prepare_data(&_test_data.rects).unwrap();
+        for p in &_test_data.points {
+            let start = Instant::now();
+            ans += algorithms::third::AlgorithmOnPersistenTree::find_single_point(
+                &_third_data_prep,
+                p,
+            );
+            let duration = start.elapsed();
+            all_p_time += duration.as_nanos();
+        }
+        print_time(&format!("({}) 3", ans), all_p_time);
     }
-
+    fn _generate_small_data() -> TestData {
+        let small_d = rand::thread_rng().gen_range(10..3000);
+        generate_tests(small_d)
+    }
+    fn _generate_medium_data() -> TestData {
+        let medium_d = rand::thread_rng().gen_range(5000..50000);
+        generate_tests(medium_d)
+    }
+    fn _generate_huge_data() -> TestData {
+        let huge_d = rand::thread_rng().gen_range(80000..200000);
+        generate_tests(huge_d)
+    }
 }
